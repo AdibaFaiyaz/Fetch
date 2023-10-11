91a60300ec56eb1d500b854724bb156f929f83b7
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2023-10-11 21:39:30 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://dummyjson.com/products`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.products);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Brand</th>         
+          <th>Price</th>
+          <th>Rating</th>
+        </tr>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.title}</td>
+            <td>{item.brand}</td>
+            <td>{item.price}</td>
+            <td>{item.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
